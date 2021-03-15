a2480733e4b16d0a2679dbc672fd363529965c1f
wilson kao <wilkao036@gmail.com>
2021-03-16 00:29:14 +0800

    add header and navigation

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,13 @@
+import Header from '../components/header';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Navigation from '../components/Navigation';
 
 const Home = () => {
   return (
     <div>
+      <Header />
+      <Navigation />
       <h1>Notedly</h1>
-      <p>This is the home page</p>
-      <ul>
-        <li>
-          <Link to="mynotes">My Notes</Link>
-        </li>
-        <li>
-          <Link to="favorites">Favorites</Link>
-        </li>
-      </ul>
     </div>
   );
 };
