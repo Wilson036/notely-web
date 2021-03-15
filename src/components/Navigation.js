81d94cca3d37d09ec82fa3c803739ad0ef7f928f
wilson kao <wilkao036@gmail.com>
2021-03-16 01:53:52 +0800

    修改layout 樣式

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledNav = styled.nav`
+  padding: 1em;
+  background: #f5f4f0;
+
+  @media (max-width: 700px) {
+    padding-top: 64px;
+  }
+
+  @media (min-width: 700px) {
+    position: fixed;
+    width: 220px;
+    height: calc(100% - 64px);
+    overflow-y: scroll;
+  }
+`;
+
+const NavList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  line-height: 2;
+
+  a {
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 1.1em;
+    color: #333;
+  }
+  a:visited {
+    color: #333;
+  }
+
+  a:hover,
+  a:focus {
+    color: #0077cc;
+  }
+`;
 
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
+    <StyledNav>
+      <NavList>
         <li>
           <span aria-hidden="true" role="img">
             🏠
@@ -23,8 +62,8 @@ const Navigation = () => {
           </span>
           <Link to="/favorites">Favorites</Link>
         </li>
-      </ul>
-    </nav>
+      </NavList>
+    </StyledNav>
   );
 };
 
