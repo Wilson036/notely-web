d1a89ac278d09c32af3671631c11b91687035966
wilson kao <wilkao036@gmail.com>
2021-03-17 01:34:40 +0800

    add note to redirect note content

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Favorites from './favorties';
 import Home from './home';
 import MyNotes from './mynotes';
+import NotePage from './note';
 
 const Pages = () => {
   return (
@@ -12,6 +13,7 @@ const Pages = () => {
         <Route exact path="/" component={Home}></Route>
         <Route path="/mynotes" component={MyNotes}></Route>
         <Route path="/favorites" component={Favorites}></Route>
+        <Route path="/note/:id" component={NotePage}></Route>
       </Layout>
     </Router>
   );
---
+++ src/pages/note.js
@@ -0,0 +1,32 @@
+import { gql, useQuery } from '@apollo/client';
+import Note from '../components/Note';
+import React from 'react';
+
+const GET_NOTE = gql`
+  query note($id: ID!) {
+    note(id: $id) {
+      id
+      createdAt
+      content
+      favoriteCount
+      author {
+        username
+        id
+        avatar
+      }
+    }
+  }
+`;
+
+const NotePage = props => {
+  const id = props.match.params.id;
+  const { data, loading, error } = useQuery(GET_NOTE, { variables: { id } });
+  if (loading) return <p>Loading....</p>;
+  if (error) {
+    console.error('err', error);
+    return;
+  }
+  return <Note note={data.note} />;
+};
+
+export default NotePage;
