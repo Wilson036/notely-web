47746c7603a586fdecb33e0d0952a751fd75c379
wilson kao <wilkao036@gmail.com>
2021-03-17 01:13:16 +0800

    styling note and noteFeed

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import { format } from 'date-fns';
+import styled from 'styled-components';
+
+const StyledNote = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+`;
+
+const MetaData = styled.div`
+  @media (min-width: 500px) {
+    display: flex;
+    align-items: top;
+  }
+`;
+const MetaInfo = styled.div`
+  padding-right: 1em;
+`;
+
+const UserAction = styled.div`
+  margin-left: auto;
+`;
 
 const Note = ({ note }) => {
   return (
-    <article key={note.id}>
-      <img
-        src={note.author.avatar}
-        alt={`${note.author.username} avatar`}
-        height="50"
-      />{' '}
-      {note.author.username} {note.createdAt} {note.favoriteCount}{' '}
+    <StyledNote>
+      <MetaInfo>
+        <img
+          src={note.author.avatar}
+          alt={`${note.author.username} avatar`}
+          height="50"
+        />
+      </MetaInfo>
+      <MetaInfo>
+        <em>by</em> {note.author.username}
+        <br /> {format(note.createdAt, 'MM/DD/YYYY')}
+      </MetaInfo>
+      <UserAction>
+        <em>Favorites:</em>
+        {note.favoriteCount}
+      </UserAction>
       <ReactMarkdown source={note.content} />
-    </article>
+    </StyledNote>
   );
 };
 
