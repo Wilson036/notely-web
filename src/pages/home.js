40c0e5fdb4f7f3fb4ceee0ff045e3de73d9bac41
wilson kao <wilkao036@gmail.com>
2021-03-17 01:45:29 +0800

    設定分頁查詢功能

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import { gql, useQuery } from '@apollo/client';
+import Button from '../components/Button';
 import React from 'react';
 import NoteFeed from '../components/NoteFeed';
 
@@ -30,7 +31,37 @@ const Home = () => {
   if (error) {
     return <div>Error!;</div>;
   }
-  return <NoteFeed notes={data.noteFeed.notes} />;
+  return (
+    <React.Fragment>
+      <NoteFeed notes={data.noteFeed.notes} />
+      {data.noteFeed.hasNextPage && (
+        <Button
+          onClick={() =>
+            fetchMore({
+              variables: {
+                cursor: data.noteFeed.cursor
+              },
+              updateQuery: (previousResult, { fetchMoreResult }) => {
+                return {
+                  noteFeed: {
+                    cursor: fetchMoreResult.noteFeed.cursor,
+                    hasNextPage: fetchMoreResult.noteFeed.hasNextPage,
+                    notes: [
+                      ...previousResult.noteFeed.notes,
+                      ...fetchMoreResult.noteFeed.notes
+                    ],
+                    __typename: 'noteFeed'
+                  }
+                };
+              }
+            })
+          }
+        >
+          Load More
+        </Button>
+      )}
+    </React.Fragment>
+  );
 };
 
 export default Home;
