132a9dcfaecefde46a7d234e2dfbbcb04e1177e5
wilson kao <wilkao036@gmail.com>
2021-03-17 17:49:15 +0800

    將jwt 設定在header中

--- src/App.js
+++ src/App.js
@@ -2,16 +2,33 @@ import Pages from './pages';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyle from './components/GlobalStyle';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 
 const App = () => {
   const uri = process.env.REACT_APP_API_URI;
+  const httplink = createHttpLink({ uri });
   //啟動快取
   const cache = new InMemoryCache();
+  //檢查是否有jwt並將使設定至header中
+  const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        authorization: localStorage.getItem('token') || ''
+      }
+    };
+  });
 
   const client = new ApolloClient({
-    uri,
+    link: authLink.concat(httplink),
     cache,
+    resolvers: {},
     //啟用開發者工具
     connectToDevTools: true
   });
